74f1f8afdc2ca2433bf4ae8b8ce28e5b125fbafc
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-01-29 16:39:27 +0100

    start the project

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-
+<Navbar></Navbar>
+<main class='relative overflow-hidden'>
+  <MainPart></MainPart>
+</main>
+<Footer></Footer>
 </template>
 <script setup>
-
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+import MainPart from './components/MainPart.vue'
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,8 @@
+<template>
+<nav class="border-2  border-red-500 flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
+
+</nav>    
+</template>
+<script setup>
+
+</script>
