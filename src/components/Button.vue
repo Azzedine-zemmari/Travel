c7d465c6af841ea225388ca960e2c8076216dd8f
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-01-29 17:51:38 +0100

    create a navbar

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button
+      :style="{
+        'padding-left': props.px + 'px',
+        'padding-right': props.px + 'px',
+        'padding-top': props.py + 'px',
+        'padding-bottom': props.py + 'px',
+        'background-color': props.bgColor,
+        'color': props.textColor
+      }"
+      class="flex items-center gap-3 justify-center rounded-full"
+    >
+    <img v-if="props.icon" :src="props.icon" alt="icon" class="w-5 h-5" />
+    <span   >{{ props.text }}</span>
+    </button>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps(['text', 'icon', 'bgColor', 'textColor', 'px', 'py']);
+  </script>
+  
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,26 @@
 <template>
-<nav class="border-2  border-red-500 flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
-
+<nav class=" flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
+<a href="/">
+    <img src="../assets/hilink-logo.svg" alt="travel app logo" width="74" height="29">
+</a>
+<ul class="hidden h-full gap-12 lg:flex">
+    <li v-for="link in NAV_LINKS" :key="link.key">
+        <a :href="link.href" class="text-[16px] font-[400] text-gray-50 flex items-center justify-center cursor-pointer pb-1.5 transition-all hover:font-bold">{{ link.label }}</a>
+      </li>
+</ul>
+<div class=" hidden lg:flex lg:items-center lg:justify-center">
+<Button 
+text="Log in"
+bgColor="black"
+textColor="white"
+px="24"
+py="14"
+icon="./src/assets/user.svg"
+/>
+</div>
 </nav>    
 </template>
 <script setup>
-
+import {NAV_LINKS} from '../constants/index'
+import Button from './Button.vue';
 </script>
