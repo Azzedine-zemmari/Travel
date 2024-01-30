d04c39317ab02fd5284eb7301fce245cebee5632
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-01-30 11:29:44 +0100

    finish the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class=" flex items-center justify-between mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative z-30 py-5">
 <a href="/">
-    <img src="../assets/hilink-logo.svg" alt="travel app logo" width="74" height="29">
+    <img src="../assets/hilink-logo.svg" alt="travel ap logo" width="74" height="29">
 </a>
 <ul class="hidden h-full gap-12 lg:flex">
     <li v-for="link in NAV_LINKS" :key="link.key">
@@ -18,9 +18,22 @@ py="14"
 icon="./src/assets/user.svg"
 />
 </div>
-</nav>    
+<img @click="handleMenu" src="../assets/menu.svg" alt="menu of travelling app" width="32" height="32" class="inline-block cursor-pointer lg:hidden">
+</nav>  
+<div v-if="Menu" class=" float-right mr-16  max-w-[200px] shadow-md lg:hidden">
+<ul class="flex flex-col rounded-lg bg-slate-100 ">
+    <li v-for="link in NAV_LINKS" :key="link.key">
+        <a :href="link.href" class="text-[16px] font-[400] text-gray-50 px-4 py-2 flex items-center justify-center cursor-pointer pb-1.5 transition-all  hover:bg-green-600 hover:text-white hover:rounded-md    hover:font-bold">{{ link.label }}</a>
+      </li>
+</ul>
+</div>  
 </template>
 <script setup>
 import {NAV_LINKS} from '../constants/index'
 import Button from './Button.vue';
+import { ref } from 'vue';
+let Menu = ref(false)
+const handleMenu = ()=>{
+    Menu.value = !Menu.value
+}
 </script>
