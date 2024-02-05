2a98669cc09f4a0735f5c61d76bd6324c132902d
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-02-05 19:37:56 +0100

    fix some errors

--- src/Chunks/feater.vue
+++ src/Chunks/feater.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="flex flex-col gap-5 items-start justify-between">
-          <img :src="picture" width="90" class="bg-green-50 p-6 rounded-full" alt="" />
+          <img :src="picture" width="90"  class="bg-green-50 p-6 rounded-full" alt="" />
           <h1 class="text-[22px] font-[600]  leading-[100%] lg:text-[40px] lg:font-[700]">
             {{ header }}
           </h1>
