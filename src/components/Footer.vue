470520ae88dbfc085d2e4bc4b908983e7ef94018
Azzedine zemmari <98091150+Azzedine-zemmari@users.noreply.github.com>
2024-02-06 12:17:47 +0100

    complete the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="max-w-[1440px] mx-auto p-4 lg:p-6 flex flex-wrap gap-36">
+  <section class="max-w-[1440px] mx-auto p-4 lg:p-6 flex flex-wrap gap-12 md:gap-24 lg:gap-36">
     <div>
       <img src="../assets/hilink-logo.svg" alt="travel ap logo" width="74" height="29" />
     </div>
@@ -9,22 +9,29 @@
         <li class="text-[16px] font-[400] hover:text-[18px]">{{ F }}</li>
       </ul>
     </div>
-    <div v-for="FooterC in FOOTER_CONTACT_INFO">
-      <h2>{{ FooterC.title }}</h2>
-      <ul v-for="CL in FooterC.links">
-        <li>{{ CL.label }}: {{ CL.value }}</li>
-      </ul>
-    </div>
-    <div v-for="SO in SOCIALS">
-      <h2>{{ SO.title }}</h2>
+    <div>
+      <h2 class="font-[700] leading-[120%] text-[22px]">{{ FOOTER_CONTACT_INFO.title }}</h2>
       <ul>
-        <li>{{ SO.links }}</li>
+        <li class="text-[16px] font-[400]" v-for="Po in FOOTER_CONTACT_INFO.links">
+          {{ Po.label }}: {{ Po.value }}
+        </li>
       </ul>
-
     </div>
-
+    <div>
+      <h2>{{ SOCIALS.title }}</h2>
+      <ul class="flex flex-row gap-3">
+        <li class='' v-for="SOC in SOCIALS.links">
+          <img :src="SOC" alt="">
+        </li>
+      </ul>
+    </div>
+    
   </section>
+  <div class="flex flex-col justify-center items-center">
+    <div class="border border-t-2 w-1/2 mx-auto border-gray-10 overflow-visible"></div>
+    <p class="mt-2 text-[16px] font-[400]">Created By <strong>Azzedine <span class="text-green-50">Zemmari</span></strong></p>
+  </div>
 </template>
 <script setup>
-import {FOOTER_LINKS,FOOTER_CONTACT_INFO,SOCIALS} from '@/constants/index'
+import {  FOOTER_LINKS , FOOTER_CONTACT_INFO , SOCIALS } from '@/constants/index'
 </script>
